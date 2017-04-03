3c9470292ecf860f01e9ac13fe16640eb023a038
UMFsimke <simic.aleksandar87@gmail.com>
2017-04-03 23:03:20 +0200

    Add sceleton for annotations

---
+++ radar-annotations/src/main/java/radar/RecyclerViewAdapter.java
@@ -0,0 +1,14 @@
+package radar;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(CLASS)
+@Target(TYPE)
+public @interface RecyclerViewAdapter {
+  Class<?>[] viewholders() default {};
+  boolean filterable() default false;
+}
---
+++ radar-annotations/src/main/java/radar/ViewHolder.java
@@ -0,0 +1,13 @@
+package radar;
+
+import android.support.annotation.StringRes;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.TYPE)
+public @interface ViewHolder {
+  @StringRes int layoutId() default 0;
+}
